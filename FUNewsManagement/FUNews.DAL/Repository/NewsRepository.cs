9cfe7db31446459ac3849b63651801d2cf48764f
haocp <haocpse184107@fpt.edu.vn>
2025-06-02 09:33:04 +0700

    init service, repo, dtos news

---
+++ FUNewsManagement/FUNews.DAL/Repository/NewsRepository.cs
@@ -0,0 +1,15 @@
+﻿using FUNews.DAL.Entity;
+using FUNews.DAL.InterfaceRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNews.DAL.Repository
+{
+    public class NewsRepository(FUNewsDbContext context) : BaseRepository<NewsArticle, String>(context), INewsRepository
+    {
+
+    }
+}
---
+++ FUNewsManagement/FuNews.Modals/DTOs/Request/News/NewsRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuNews.Modals.DTOs.Request.News
+{
+    public class NewsRequest
+    {
+        public string? NewsTitle { get; set; }
+        public string? Headline { get; set; }
+        public string? NewsContent { get; set; }
+        public string? NewsSource { get; set; }
+        public short? CategoryId { get; set; }
+        public bool? NewsStatus { get; set; }
+        public List<int>? TagIds { get; set; }
+
+    }
+}
